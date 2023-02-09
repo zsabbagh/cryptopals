1eb3686434e3de2e0dd502bcc8aa140a87b98758
zsabbagh <zassabbagh@gmail.com>
2023-02-09 17:01:45 +0100

    Add progress to key finder

--- 2-set/main.py
+++ 2-set/main.py
@@ -7,6 +7,7 @@ import os
 from Crypto.Cipher import AES
 import argparse
 import random
+import base64
 
 # Parse arguments
 parser = argparse.ArgumentParser(description="Solves Set 2 of Cryptopals")
@@ -23,7 +24,24 @@ parser.add_argument("--random", nargs='?', type=int, help="random data", const=1
 parser.add_argument("-x", "--hex", help="output and force-read hex", action="store_true")
 parser.add_argument("-d", "--decrypt", action="store_true", help="decryption mode for ECB/CBC assignment")
 parser.add_argument("-e", "--encrypt", action="store_true", help="encryption mode for ECB/CBC assignment")
+parser.add_argument('-l', "--len", type=int, help="size of random hex key", default=16)
+parser.add_argument('-w', "--wait", type=float, help="wait seconds between instructions", default=0.1)
 args = parser.parse_args()
+
+def wait():
+    time.sleep(args.wait)
+
+def random_hex(length: int=16, upper: bool=False) -> str:
+    """
+        Returns a random hex string.
+    """
+    res = os.urandom(length).hex()[:length]
+    if upper:
+        return res.upper()
+    return res
+
+RANDOM_KEY = random_hex(args.len).encode('ascii')
+ASSIGNMENT_TWELVE_STRING = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
 
 def is_hex(s: str):
     value = True
@@ -137,23 +155,31 @@ def is_ecb(data: bytes, block_length: int=AES.block_size) -> bool:
     return False
 
 
-def encoding_oracle(data, key, iv, random_bytes=False, span=(5,10)):
+def encrypting_oracle(data, key=RANDOM_KEY, iv=b'00000000', random_bytes=False, span=(5,10)):
     """
         Randomise encryption of ECB / CBC
         Returns (data_encrypted, is_encrypted_with_cbc)
+        If Assignment == '12', it encrypts with ECB
     """
-    if random_bytes:
-        prefix = os.urandom(random.randint(span[0], span[1]))
-        suffix = os.urandom(random.randint(span[0], span[1]))
+    if args.assignment == '12':
+        new = bytes(base64.b64decode(ASSIGNMENT_TWELVE_STRING))
+        data += new
+        out = ecb_encrypt(data, key)
+        return out, True
     else:
-        prefix = b'0' * random.randint(span[0], span[1])
-        suffix = b'0' * random.randint(span[0], span[1])
-    data = prefix + data + suffix
+        if random_bytes:
+            prefix = os.urandom(random.randint(span[0], span[1]))
+            suffix = os.urandom(random.randint(span[0], span[1]))
+        else:
+            prefix = b'0' * random.randint(span[0], span[1])
+            suffix = b'0' * random.randint(span[0], span[1])
+        data = prefix + data + suffix
     is_ecb_encrypted = random.randint(0, 1) == 0
     if is_ecb_encrypted:
         return ecb_encrypt(data, key), True
     else:
         return cbc_encrypt(data, key, iv), False
+        
 
 def initialise():
     """
@@ -198,12 +224,17 @@ def run_tests(data, key, iv):
     print("...OK")
     print("\nAll tests OK!")
 
+def single_byte(i: int):
+    return int.to_bytes(i, 1, 'little')
+
 def main():
     # Seed PRNG
     random.seed(int.from_bytes(os.urandom(4), 'little'))
     # Set data
     data, key, iv = initialise()
     # Check assignment
+    if args.assignment in ['hex', 'hexlen']:
+        print(RANDOM_KEY)
     if args.assignment == 'tests':
         run_tests(data, key, iv)
     if args.assignment == 'cbc':
@@ -233,7 +264,7 @@ def main():
         count_ecb_guesses = 0
         count_ecb_actuals = 0
         for _ in range(args.times):
-            encrypted_data, actual = encoding_oracle(data, key, iv)
+            encrypted_data, actual = encrypting_oracle(data, key, iv)
             guessed = is_ecb(encrypted_data)
             if guessed:
                 count_ecb_guesses += 1
@@ -244,6 +275,58 @@ def main():
         print(f"Guessed correct {guessed_correct} / {args.times} ({100*guessed_correct/float(args.times)}%)")
         print(f"...where {count_ecb_guesses} guesses were ECB")
         print(f"...where {count_ecb_actuals} actuals were ECB")
+    if args.assignment == '12':
+        # Check data
+        prev_len = -1
+        curr_input = b'A'
+        block_len = None
+        # Find block length
+        while block_len is None:
+            encrypted, _ = encrypting_oracle(curr_input)
+            if len(encrypted) > prev_len and prev_len > 0:
+                block_len = len(encrypted) - prev_len
+                break
+            else:
+                prev_len = len(encrypted)
+            curr_input += b'A'
+        print(f"Block length {block_len}")
+        # Min blocks
+        encrypted, _ = encrypting_oracle(b'')
+        min_blocks = len(encrypted) // block_len
+        print(f"Encrypted len: {len(encrypted)}")
+        print(f"Minimum blocks: {min_blocks}")
+        # Check ECB encoding
+        is_ecb_encoded = is_ecb(encrypting_oracle(b'A'*2*block_len)[0])
+        print(f"Is ECB encoded: {is_ecb_encoded}")
+        # Trying to find key on last pos
+        total = b''
+        for round in range(0, min_blocks):
+            filler = b'A' * block_len * round
+            get_block = lambda x : x[block_len*(min_blocks-1):block_len*min_blocks]
+            result = b''
+            for n in range(1, block_len):
+                nfew_bytes = filler + total + b'A' * (block_len - n)
+                tracker = {}
+                # Go through all possible bytes
+                for i in range(256):
+                    byte_val = single_byte(i)
+                    curr_input = nfew_bytes + result + byte_val
+                    encrypted, _ = encrypting_oracle(curr_input)
+                    tracker[curr_input] = encrypted
+                output, _ = encrypting_oracle(nfew_bytes)
+                found = False
+                for (curr_input, encrypted) in tracker.items():
+                    if get_block(encrypted) == get_block(output):
+                        found = True
+                        result += single_byte(curr_input[-1])
+                        break
+                if args.verbose and not found:
+                    print(f"Not found for {n}")
+            print(f"\nResult: {result.decode('ascii')}")
+            total += result
+        print(f"\nTotal: {total.decode('ascii')}")
+
+
 
 if __name__ == "__main__":
     main()
